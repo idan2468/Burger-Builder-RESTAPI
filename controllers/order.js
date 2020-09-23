c398dc5b2ad82cec41507da073c912a391d475da
idan2468 <idans2468@gmail.com>
2020-09-23 23:48:58 +0300

    Updated order controller to populate customer details

--- controllers/order.js
+++ controllers/order.js
@@ -2,7 +2,13 @@ const Order = require('../models/order');
 const Customer = require('../models/customer')
 // GET => /orders
 exports.getOrders = async (req, res, next) => {
-    console.log('GetOrder');
+    try {
+        const orders = await Order.find().populate('customer');
+        console.log(orders);
+        res.status(200).json(orders.map(order => order._doc));
+    } catch (e) {
+        throw new Error(e);
+    }
 }
 
 // PUT => /order
@@ -18,8 +24,7 @@ exports.createOrder = async (req, res, next) => {
         const newOrder = new Order({price: price, burgerIngredients: ingredients, customer: customer._id});
         await newOrder.save();
         return res.status(200).json("success");
-    }
-    catch (err) {
+    } catch (err) {
         next(new Error(err));
     }
 }
