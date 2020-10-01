49df933cadf19a7d27b03c4d4aace10462be603c
idan2468 <idans2468@gmail.com>
2020-10-01 10:58:44 +0300

    Added user model

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = new require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    isAdmin: {type: Boolean, default: false}
+}, {timestamps: true});
+
+module.exports = mongoose.model('user', userSchema);
